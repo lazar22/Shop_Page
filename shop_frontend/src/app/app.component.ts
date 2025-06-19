3890e2c12d0c3d1c34b2cad993fc9bf297a65d2f
lazar22 <roksandiclazar@gmail.com>
2025-06-19 06:48:02 +0200

    Added simple navbar with working toggle for dark/light mode

--- shop_frontend/src/app/app.component.ts
+++ shop_frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {WelcomeScreenComponent} from "./welcome-screen/welcome-screen.component";
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeScreenComponent],
+  imports: [RouterOutlet, WelcomeScreenComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- shop_frontend/src/app/nav-bar/nav-bar.component.ts
+++ shop_frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {Component, HostListener, Inject, Renderer2} from '@angular/core';
+
+export type Theme = 'light_mode' | 'dark_mode';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
 
+export class NavBarComponent {
+  theme: Theme = 'dark_mode';
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private renderer: Renderer2) {
+  }
+
+  mode_change() {
+    const icon = document.querySelector(".mode") as HTMLElement;
+    const newTheme: Theme = (this.theme === 'dark_mode') ? 'light_mode' : 'dark_mode';
+
+    icon.style.backgroundImage = (newTheme === 'dark_mode') ? ('url("/icons/moon.png")') : ('url("/icons/sun.png")');
+    this.document.body.classList.replace(this.theme, newTheme);
+
+    this.theme = newTheme;
+  }
 }
